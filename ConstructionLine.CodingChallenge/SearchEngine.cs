340d916a8caa3a05bee2db1dd6f9e048adae0b60
Darren <darren@inject-it.co.uk>
2020-01-22 09:04:07 +0000

    Implement a pre-sorted color/size Dictionary and then perform the appropriate set based operations

--- ConstructionLine.CodingChallenge/SearchEngine.cs
+++ ConstructionLine.CodingChallenge/SearchEngine.cs
@@ -1,26 +1,40 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System;
 
 namespace ConstructionLine.CodingChallenge
 {
     public class SearchEngine
     {
         private readonly List<Shirt> _shirts;
+        private readonly IDictionary<Guid, List<Shirt>> _shirtLookup = new Dictionary<Guid, List<Shirt>>();
 
         public SearchEngine(List<Shirt> shirts)
         {
             _shirts = shirts;
 
-            // TODO: data preparation and initialisation of additional data structures to improve performance goes here.
-
+            // Filter all colours and sizes into a pre-processed dictionary for fast lookups e.g. red -> (set of red shirts)
+            Color.All.ForEach(color => _shirtLookup.Add(color.Id, _shirts.Where(shirt => shirt.Color.Id == color.Id).ToList()));
+            Size.All.ForEach(size => _shirtLookup.Add(size.Id, _shirts.Where(shirt => shirt.Size.Id == size.Id).ToList()));
         }
 
-
         public SearchResults Search(SearchOptions options)
-        {
-            // TODO: search logic goes here.
+        {          
+            var setOfRequiredColors = options.Colors.Any()
+                ? options.Colors.SelectMany(color => _shirtLookup[color.Id])
+                : Color.All.SelectMany(color => _shirtLookup[color.Id]);
 
+            var setOfRequiredSizes = options.Sizes.Any()
+                ? options.Sizes.SelectMany(size => _shirtLookup[size.Id])
+                : Size.All.SelectMany(size => _shirtLookup[size.Id]);
+
+            var setOfAllMatchingShirts = setOfRequiredColors.Intersect(setOfRequiredSizes).ToList();
+      
             return new SearchResults
             {
+                Shirts = setOfAllMatchingShirts,
+                ColorCounts = Color.All.Select(color => new ColorCount { Color = color, Count = setOfAllMatchingShirts.Select(o => o.Color.Id).Count(o => o == color.Id) }).ToList(),
+                SizeCounts = Size.All.Select(size => new SizeCount { Size = size, Count = setOfAllMatchingShirts.Select(o => o.Size.Id).Count(o => o == size.Id) }).ToList()
             };
         }
     }
